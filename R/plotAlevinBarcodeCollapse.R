d8101285f4fda608bf5731d1bd1d5769150bd8fc
Charlotte Soneson <charlottesoneson@gmail.com>
2018-12-18 17:51:28 +0100

    Remove color in barcode collapse plot

--- R/plotAlevinBarcodeCollapse.R
+++ R/plotAlevinBarcodeCollapse.R
@@ -1,21 +1,17 @@
 #' Summary plot of cell barcode collapsing
 #'
 #' Plot the original frequency of each cell barcode in the original whitelist
-#' against the frequency after collapsing similar cell barcodes. Points are
-#' colored based on whether the corresponding cell barcodes are retained in the
-#' final whitelist or not.
+#' against the frequency after collapsing similar cell barcodes.
 #'
 #' @author Charlotte Soneson
 #'
 #' @param cbTable \code{data.frame} (such as the \code{cbTable} returned by
-#'   \code{readAlevinQC}) with barcode frequencies before and after collapsing,
-#'   as well as a column indicating whether the barcode is retained in the final
-#'   whitelist.
+#'   \code{readAlevinQC}) with barcode frequencies before and after collapsing.
 #'
 #' @export
 #'
 #' @importFrom ggplot2 ggplot aes geom_abline geom_point theme_bw theme
-#'   scale_color_manual element_text xlab ylab
+#'   element_text xlab ylab
 #' @import dplyr
 #'
 #' @return A ggplot object
@@ -27,14 +23,11 @@
 #'
 plotAlevinBarcodeCollapse <- function(cbTable) {
     ggplot2::ggplot(cbTable %>% dplyr::filter(inFirstWhiteList),
-                    ggplot2::aes(x = originalFreq, y = collapsedFreq,
-                                 color = inFinalWhiteList)) +
+                    ggplot2::aes(x = originalFreq, y = collapsedFreq)) +
         ggplot2::geom_abline(slope = 1, intercept = 0) +
         ggplot2::geom_point() +
         ggplot2::theme_bw() +
         ggplot2::theme(axis.title = ggplot2::element_text(size = 12)) +
-        ggplot2::scale_color_manual(values = c(`TRUE` = "red",
-                                               `FALSE` = "darkgreen")) +
         ggplot2::xlab("Cell barcode frequency, original whitelist") +
         ggplot2::ylab("Cell barcode frequency, following reassignment")
 }
