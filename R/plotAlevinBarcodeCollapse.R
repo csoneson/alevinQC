6a68713618d4c96cefc0d6d5cbd67d6beb139ccd
Charlotte Soneson <charlottesoneson@gmail.com>
2018-12-18 18:13:25 +0100

    Add average read gain per CB to barcode collapse plot

--- R/plotAlevinBarcodeCollapse.R
+++ R/plotAlevinBarcodeCollapse.R
@@ -11,7 +11,7 @@
 #' @export
 #'
 #' @importFrom ggplot2 ggplot aes geom_abline geom_point theme_bw theme
-#'   element_text xlab ylab
+#'   element_text xlab ylab geom_label
 #' @import dplyr
 #'
 #' @return A ggplot object
@@ -22,6 +22,9 @@
 #' plotAlevinBarcodeCollapse(alevin$cbTable)
 #'
 plotAlevinBarcodeCollapse <- function(cbTable) {
+    mrg <- cbTable %>% dplyr::filter(inFirstWhiteList) %>%
+        dplyr::summarize(mrg = signif(100 * mean(collapsedFreq/originalFreq - 1), 4)) %>%
+        dplyr::pull(mrg)
     ggplot2::ggplot(cbTable %>% dplyr::filter(inFirstWhiteList),
                     ggplot2::aes(x = originalFreq, y = collapsedFreq)) +
         ggplot2::geom_abline(slope = 1, intercept = 0) +
@@ -29,5 +32,7 @@ plotAlevinBarcodeCollapse <- function(cbTable) {
         ggplot2::theme_bw() +
         ggplot2::theme(axis.title = ggplot2::element_text(size = 12)) +
         ggplot2::xlab("Cell barcode frequency, original whitelist") +
-        ggplot2::ylab("Cell barcode frequency, following reassignment")
+        ggplot2::ylab("Cell barcode frequency, following reassignment") +
+        ggplot2::geom_label(x = -Inf, y = Inf, hjust = -0.05, vjust = 1.3,
+                            aes(label = paste0("Mean read gain per CB: ", mrg, "%")))
 }
