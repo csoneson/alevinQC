8a87896f562a73eb76134a5e23cb1addbe038b32
Charlotte Soneson <charlottesoneson@gmail.com>
2020-08-27 22:50:46 +0200

    Fix examples

--- R/plotAlevinQuantPairs.R
+++ R/plotAlevinQuantPairs.R
@@ -18,7 +18,7 @@
 #' @return A ggmatrix object
 #'
 #' @examples
-#' alevin <- readAlevinQC(system.file("extdata/alevin_example_pre0.14",
+#' alevin <- readAlevinQC(system.file("extdata/alevin_example_v0.14",
 #'                                    package = "alevinQC"))
 #' plotAlevinQuantPairs(alevin$cbTable, colName = "inFinalWhiteList")
 #'
