89a98a56e6d6de4dac67f15a774cd2991675e20c
Charlotte Soneson <charlottesoneson@gmail.com>
2021-01-31 17:44:17 +0100

    Fix .checkPandoc function

--- R/alevinQCReport.R
+++ R/alevinQCReport.R
@@ -12,7 +12,10 @@
 #'   available.
 #'
 .checkPandoc <- function(ignorePandoc) {
-    if (Sys.which("pandoc") == "") {
+    ## First check whether pandoc is available
+    if (!rmarkdown::pandoc_available()) {
+        ## If pandoc is not available, either give a warning or an error,
+        ## depending on the value of ignorePandoc
         if (ignorePandoc) {
             ## If ignorePandoc is TRUE, just give a warning
             warning("pandoc is not available! ",
@@ -21,15 +24,27 @@
             ## If ignorePandoc is FALSE, stop
             stop("pandoc is not available!")
         }
-    }
-    if (Sys.which("pandoc-citeproc") == "") {
-        if (ignorePandoc) {
-            ## If ignorePandoc is TRUE, just give a warning
-            warning("pandoc-citeproc is not available! ",
-                    "The final report will not be generated.")
-        } else {
-            ## If ignorePandoc is FALSE, stop
-            stop("pandoc-citeproc is not available!")
+    } else {
+        ## If pandoc is available, check for pandoc-citeproc
+        ## Only do this if the pandoc version is <2.11, since
+        ## pandoc-citeproc is not included (or needed) in v2.11 and later.
+        if (!rmarkdown::pandoc_available(version = "2.11")) {
+            ## TRUE if the available pandoc version is not 2.11 or newer
+            ## pandoc-citeproc should be found in the path, or in the
+            ## same folder as the pandoc executable
+            if (Sys.which("pandoc-citeproc") == "" &&
+                !file.exists(file.path(dirname(rmarkdown::pandoc_exec()),
+                                       "pandoc-citeproc"))) {
+                ## pandoc-citeproc is required, but not found
+                if (ignorePandoc) {
+                    ## If ignorePandoc is TRUE, just give a warning
+                    warning("pandoc-citeproc is not available! ",
+                            "The final report will not be generated.")
+                } else {
+                    ## If ignorePandoc is FALSE, stop
+                    stop("pandoc-citeproc is not available!")
+                }
+            }
         }
     }
 }
