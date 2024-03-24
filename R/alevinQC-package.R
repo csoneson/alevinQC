40ac72e2333aaef603ed2dc062ef160ed4b74349
Charlotte Soneson <charlottesoneson@gmail.com>
2024-03-24 15:54:44 +0100

    Update package documentation format

--- R/alevinQC-package.R
+++ R/alevinQC-package.R
@@ -2,13 +2,12 @@
 #'
 #' alevinQC
 #'
-#' @name alevinQC-pkg
-#' @docType package
 ## usethis namespace: start
 #' @useDynLib alevinQC, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 globalVariables(c("V1", "V2", "V3", "V4", "V5", "CB",
                   "inFirstWhiteList", "inFinalWhiteList",
